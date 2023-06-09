61917b6257a9ab2eccab838350d18655bfc4d741
hellomaryjo <majorx3music@gmail.com>
2023-06-08 21:58:47 -0500

    working on rsvp

--- src/components/RSVP.tsx
+++ src/components/RSVP.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import ResponseForm from "./ResponseForm";
 import SearchForm from "./SearchForm";
 
-// figure out how state will be managed
+// TODO: figure out how state will be managed
+// TODO: should allow user to search for name or name in their party
+// TODO: connect to google sheets and make GET request to search for name
 
 const RSVP = () => {
   return (
