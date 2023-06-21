a0aaadca15f338d9b5f6515f13ee0593a0f847f9
hellomaryjo <majorx3music@gmail.com>
2023-06-20 19:55:58 -0500

    refactored our story imgs to use astrojs image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), image()]
 });
