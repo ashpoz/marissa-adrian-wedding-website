484ddcc7b4f19b2268430e2ebf1970733f528992
hellomaryjo <majorx3music@gmail.com>
2023-06-22 14:10:42 -0500

    update config for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
-
 import netlify from "@astrojs/netlify/functions";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react(), image()],
-  adapter: netlify()
+  adapter: vercel()
 });
