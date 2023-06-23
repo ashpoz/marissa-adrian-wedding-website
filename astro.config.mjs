46a21b0c5efca66ac80dcc2d4f39882a9af5d2f8
hellomaryjo <majorx3music@gmail.com>
2023-06-23 12:52:46 -0500

    removed swiper js and refactored timeline with scroll animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
-import netlify from "@astrojs/netlify/functions";
 
 import vercel from "@astrojs/vercel/serverless";
 
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,3 +1,28 @@
+// normalizeString: removes accents from a string
 export const normalizeString = (str: string) => {
   return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+};
+
+// checks if the device is mobile
+export const isMobile = () => {
+  let hasTouchScreen = false;
+  if ("maxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+  } else if ("msMaxTouchPoints" in navigator) {
+    hasTouchScreen = (navigator as any).msMaxTouchPoints > 0;
+  } else {
+    const mQ = matchMedia?.("(pointer:coarse)");
+    if (mQ?.media === "(pointer:coarse)") {
+      hasTouchScreen = !!mQ.matches;
+    } else if ("orientation" in window) {
+      hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+      // Only as a last resort, fall back to user agent sniffing
+      const UA = (navigator as any).userAgent;
+      hasTouchScreen =
+        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+        /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+    }
+  }
+  return hasTouchScreen;
 };
