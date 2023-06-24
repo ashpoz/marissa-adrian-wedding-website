871dd05dbd3c2860f9249b4193951c6ac5c44ae2
hellomaryjo <majorx3music@gmail.com>
2023-06-24 16:54:31 -0500

    add some button hover styles, still working on this

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,12 +23,24 @@ module.exports = {
 				},
 				"almond": "#EDE2D6",
 				"dark-slate-gray": "#37514D",
-				"forest-green": "#6A715D",
-				"redwood": "#B6594C",
-				"burgundy": "#91453B",
 				"crimson": "#880D1E",
 				"white": "#fff",
-				"black": "151612",
+				"black": "#151612",
+				"forest-green": {
+					DEFAULT: "#6A715D",
+					light: "#7E876E",
+					dark: "#545A49",
+				},
+				"redwood": {
+					DEFAULT: "#B6594C",
+					light: "#BE6B60",
+					dark: "#9F4C41"
+				},
+				"burgundy": {
+					DEFAULT: "#91453B",
+					light: "#AE5347",
+					dark: "#823E35"
+				},
 			},
 			typography: {
 				DEFAULT: {
