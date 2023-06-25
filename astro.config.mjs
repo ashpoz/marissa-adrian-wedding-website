0b2ebb73641e9cf8585821aa0902dba63330acee
hellomaryjo <majorx3music@gmail.com>
2023-06-25 16:10:05 -0500

    now I think I get hybrid render, astro config set to server, then add prerender to true to static pages, also converted md to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), react(), image({
+  integrations: [tailwind(), mdx(), react(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
   })],
   adapter: vercel()
