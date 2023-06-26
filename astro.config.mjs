c41a6fd9ec702b39bb987bad35a287e3c2a6fc93
hellomaryjo <majorx3music@gmail.com>
2023-06-26 18:21:15 -0500

    added partytown to see if that could help with GA load times

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,17 @@ import react from "@astrojs/react";
 import image from "@astrojs/image";
 import vercel from "@astrojs/vercel/serverless";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), mdx(), react(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
+  }), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
   })],
   adapter: vercel()
 });
